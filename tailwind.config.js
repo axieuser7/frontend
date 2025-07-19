5dee7c0bf55ed876d11f90d0868b64cef887f210
axieuser10 <user10@jonkoping.site>
2025-07-19 16:06:57 +0200

    Fix missing bg-background and text-foreground classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ export default {
     extend: {
       colors: {
         border: 'hsl(214.3 31.8% 91.4%)',
+        background: 'hsl(0 0% 100%)',
+        foreground: 'hsl(222.2 84% 4.9%)',
       },
     },
   },
